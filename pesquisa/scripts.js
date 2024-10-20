b011129f141610db3c61f575dbbe22a88f3e8c30
wendellnd <wendelldourado.wn@gmail.com>
2024-10-20 15:35:42 -0300

    feat: implement search

--- pesquisa/scripts.js
+++ pesquisa/scripts.js
@@ -1,3 +1,27 @@
 function exit() {
   window.location.href = "../index.html";
 }
+
+function search() {
+  var search = document.getElementById("search").value;
+  const items = document.getElementsByClassName("categoryItem");
+
+  if (search !== "") {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      const title = item.getElementsByClassName("categoryName")[0];
+      const titleText = title.innerText;
+
+      if (titleText.toLowerCase().includes(search.toLowerCase())) {
+        item.style.display = "flex";
+      } else {
+        item.style.display = "none";
+      }
+    }
+  } else {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      item.style.display = "flex";
+    }
+  }
+}
