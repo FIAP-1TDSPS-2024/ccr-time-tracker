18e926b28fa385796b06a1e1b10e48bfce4dab45
wendellnd <wendelldourado.wn@gmail.com>
2024-10-20 16:31:35 -0300

    feat: create items with js

--- pesquisa/scripts.js
+++ pesquisa/scripts.js
@@ -25,3 +25,55 @@ function search() {
     }
   }
 }
+
+const allItems = [
+  {
+    name: "TMP",
+    icon: "../img/clock.svg",
+    favorite: true,
+  },
+  {
+    name: "TEPP",
+    icon: "../img/train.svg",
+  },
+];
+
+function createItems(categoryName) {
+  const categoryList = document.getElementById(categoryName);
+
+  let categoryItems = [];
+  if (categoryName == "favorites") {
+    categoryItems = allItems.filter((item) => item.favorite);
+  } else {
+    categoryItems = allItems;
+  }
+
+  for (let i = 0; i < categoryItems.length; i++) {
+    const categoryItem = categoryItems[i];
+
+    const item = document.createElement("div");
+    item.className = "categoryItem";
+    item.innerHTML = `
+      <img src="${categoryItem.icon}" class="categoryIcon">
+      <img class="star" src="${
+        categoryItem.favorite ? "../img/star.svg" : "../img/star-disabled.svg"
+      }" />
+      <div class="categoryName"><a>${categoryItem.name}</a></div>
+    `;
+
+    categoryList.appendChild(item);
+  }
+
+  // Add item button
+  if (categoryName == "all") {
+    const addItem = document.createElement("div");
+    addItem.className = "addItem shadow";
+    addItem.innerHTML = `
+    <a>+</a>
+  `;
+    categoryList.appendChild(addItem);
+  }
+}
+
+createItems("all");
+createItems("favorites");
